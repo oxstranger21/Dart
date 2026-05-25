62a7503419e187b0839689656ff8f1a6b4cb64d8
oxstranger21 <gopi2023@gift.edu.in>
2026-05-25 23:32:56 +0530

    Add initial implementation of main function with variable declarations and print statements

---
+++ variable.dart
@@ -0,0 +1,14 @@
+void main(){
+  int a = 10;
+  double b = 3.14;
+  String c = "Hello, Dart!";
+  bool d = true;
+  var e = "I can be any type!";
+
+  print("Integer: $a");
+  print("Double: $b");
+  print("String: $c");
+  print("Boolean: $d");
+  print("Dynamic: $e");
+  
+}
