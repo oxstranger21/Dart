d16f07cedcb604c1dd3d3a5af8f6d6c4d22d32f8
oxstranger21 <gopi2023@gift.edu.in>
2026-05-31 14:12:33 +0530

    if or else statement

---
+++ if_else.dart
@@ -0,0 +1,26 @@
+void main(){
+  
+  
+  int a = 5;
+  int b = 6;
+  int c = 7;
+  
+  
+  if(a>b){
+    if(a>c){
+      print('$a is grater');
+    }
+    else{
+      print('$c is greater ');
+    }
+ }
+  else{
+    if(b>c){
+    print('$b is grater ');
+    }
+    
+    else{
+      print('$c is grater ');
+    }
+}
+}
