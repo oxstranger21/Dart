cdea6b1c61e23ef8acd5f4368e72c9e7b5999176
oxstranger21 <gopi2023@gift.edu.in>
2026-05-31 14:36:11 +0530

    for loop

---
+++ for_loop.dart
@@ -0,0 +1,11 @@
+void main (){
+  List <int> nums = [1,2,3,4,5,4];
+  
+  for(int i = 0; i<=5; i++){
+    print(nums[i]);
+  }
+  print ('secound loop ------');
+  for(int num in nums){
+    print(num);
+  }
+}
